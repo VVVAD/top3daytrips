205a8b6595ea65b11e2bc230b9c92624161fa939
vvvad <Vadim.gavrilov@s.afeka.ac.il>
2024-04-24 17:52:07 +0300

    final 7

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ document.body.addEventListener('click', function (event) {
 function isHomePage() {
   // Check whatever conditions necessary to determine if it's the home page
   // For example, you can check the current URL
-  return window.location.pathname.includes('/index.html') // Adjust the URL as needed
+  return window.location.pathname.includes('/index.html');// Adjust the URL as needed
 }
 
 // Resize the width of the .menu-image
